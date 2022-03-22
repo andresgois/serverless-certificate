93ff71aad16a45736ce6b8f6bc895f964cc39dd1
Andre gois <andre.s.gois3@gmail.com>
2022-03-22 20:37:16 -0300

    :sparkles: include template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,5 +1,11 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "src/utils/dynamodbClient";
+import { compile } from "handlebars";
+import * as dayjs from "dayjs";
+
+import { join } from "path";
+import { readFileSync } from "fs";
+import * as chromium from "chrome-aws-lambda";
 
 interface ICreateCertificate {
   id: string;
@@ -7,11 +13,24 @@ interface ICreateCertificate {
   grade: string;
 }
 
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificates.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+};
+
 export const handler: APIGatewayProxyHandler = async (event) => {
-  /**
-   * Vai receber
-   * id, name, grade
-   */
+  /** Vai receber: id, name, grade   */
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
   await document
@@ -35,6 +54,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal,
+  };
+
+  const content = await compileTemplate(data);
 
   return {
     statusCode: 201,
