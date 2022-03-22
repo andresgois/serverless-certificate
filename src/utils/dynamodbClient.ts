4b124c1dc25574934919328e76badcffabdbea8c
Andre gois <andre.s.gois3@gmail.com>
2022-03-21 21:09:58 -0300

    :wrench: config database

---
+++ src/utils/dynamodbClient.ts
@@ -0,0 +1,21 @@
+import { DynamoDB } from "aws-sdk";
+
+const options = {
+  region: "localhost",
+  endpoint: "http://localhost:8000",
+  accessKeyId: "x",
+  secretAccessKey: "x",
+};
+/**
+ * Apenas para teste local
+ * accessKeyId: "x",
+ * secretAccessKey: "x",
+ */
+
+const isOffiline = () => {
+  return process.env.IS_OFFLINE;
+};
+
+export const document = isOffiline()
+  ? new DynamoDB.DocumentClient(options)
+  : new DynamoDB.DocumentClient();
