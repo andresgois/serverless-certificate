94db4ee1833300de14e67de0372a7e630ae3f85f
Andre gois <andre.s.gois3@gmail.com>
2022-03-23 21:09:19 -0300

    :sparkles: verify aws certificate

---
+++ src/functions/verifyCertificate.ts
@@ -0,0 +1,44 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "src/utils/dynamodbClient";
+
+interface IUserCertificate {
+  id: string;
+  name: string;
+  grade: string;
+  created_at: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  // http://localhost:3000/verifyCertificate/${id}
+  const { id } = event.pathParameters;
+
+  const response = await document
+    .query({
+      TableName: "users_certificate",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": id,
+      },
+    })
+    .promise();
+
+  const userCertificate = response.Items[0] as IUserCertificate;
+
+  if (userCertificate) {
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: "Certificado válido",
+        name: userCertificate.name,
+        url: `http://certificateignite2021.s3.amazonaws.com/${id}.pdf`,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Certificado inválido",
+    }),
+  };
+};
